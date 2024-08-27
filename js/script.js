d645167c1fbca5ce01161c516db8efd9def299df
armandotruglio <armando.truglio1@gmail.com>
2024-08-27 18:28:41 +0200

    Completed input part, Esercizio finito

--- js/script.js
+++ js/script.js
@@ -3,9 +3,12 @@ const memorizeSection = document.getElementById('memorize-section');
 const resultTimer = document.getElementById('timer');
 const resultRandomNumbers = document.getElementById('numbers');
 const inputSection = document.getElementById('input-section');
+const form = document.querySelector('form');
+const resultInputNumbers = document.querySelectorAll('input');
+const button  = document.querySelector('button');
 
 let counter = 30;
-
+const maxNumbers = 5;
 const randomNumbers = [];
 
 //Ogni secondo si aggiorna il timer
@@ -15,7 +18,7 @@ const interval = setInterval(() => {
 
 
 //Dopo 30 secondi lo stoppo
-setTimeout( () => {
+setTimeout(() => {
     clearInterval(interval);
 
     //Faccio sparire la sezione di memorizzazzione
@@ -27,11 +30,11 @@ setTimeout( () => {
 }, 30000);
 
 //Genero 5 numeri casuali diversi e li inserisco in pagina
-while(randomNumbers.length < 5){
+while (randomNumbers.length < maxNumbers) {
     const num = Math.floor(Math.random() * 100) + 1;
     console.log(num);
 
-    if(!randomNumbers.includes(num)){
+    if (!randomNumbers.includes(num)) {
         randomNumbers.push(num);
 
         const element = document.createElement('li');
@@ -39,3 +42,28 @@ while(randomNumbers.length < 5){
         resultRandomNumbers.appendChild(element);
     }
 }
+
+//Acquisisco i 5 numeri dal form
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log(randomNumbers);
+    const matchedNumbers = [];
+
+    for (let i = 0; i < maxNumbers; i++) {
+        num = parseInt(resultInputNumbers[i].value);
+        console.log(num);
+        if (randomNumbers.includes(num)) {
+            matchedNumbers.push(num);
+        }
+    }
+
+    const result = document.createElement('p');
+
+    result.innerText = matchedNumbers.length != 0 ? 
+    `Hai indovinato i numeri ${matchedNumbers}` 
+    : `Non hai ricordato nessun numero`
+
+    inputSection.appendChild(result);
+
+})
